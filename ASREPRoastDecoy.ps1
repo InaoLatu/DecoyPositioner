afc6dc6701af2398dc5cbdf3c61672368b8fdf1b
unknown <Administrator@companydomain.local>
2023-04-10 15:46:57 +0200

    feat:add random password to decoy users

--- ASREPRoastDecoy.ps1
+++ ASREPRoastDecoy.ps1
@@ -1,8 +1,8 @@
 # Creating decoy user for ASREPRoast attack
 
 Import-Module ActiveDirectory
-$password = "Password1" | ConvertTo-SecureString -AsPlainText -Force
 
+# Check if decoy user already exists
 try {
     $user = Get-ADUser -Identity j.anderson
     $UserExists = $true
@@ -15,6 +15,25 @@ if ($UserExists) {
     Write-Host "ASSREPRoast decoy was already deployed."
     exit
 }
+
+# Generate random password
+Function Get-RandomAlphanumericString {
+	
+	[CmdletBinding()]
+	Param (
+        [int] $length = 32
+	)
+
+	Begin{
+	}
+
+	Process{
+        Write-Output ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $length  | % {[char]$_}) )
+	}	
+}
+
+$password_aux= Get-RandomAlphanumericString | Tee-Object -variable teeTime
+$password = ConvertTo-SecureString -String $password_aux -AsPlainText -Force
 
 $user = @{
     Name = "John Anderson"
@@ -31,7 +50,12 @@ New-ADUser @user
 
 Set-ADAccountControl -Identity j.anderson -DoesNotRequirePreAuth $True
 
+Add-ADGroupMember -Identity "Domain Admins" -Members "j.anderson"  # to execute an initial login
+
+# To update the value of LastLogon to make the decoy user more realistic
 $username = 'COMPANYDOMAIN\j.anderson'
 $credential = New-Object System.Management.Automation.PSCredential $username, $password
-$ActivateLastLogon = Start-Process powershell.exe -Credential $credential -PassThru -ArgumentList '-WindowStyle Hidden'
+$ActivateLastLogon = Start-Process powershell.exe -Credential $credential -PassThru -ArgumentList '-WindowStyle Hidden' -WorkingDirectory 'C:\Windows\System32'
 Stop-Process $ActivateLastLogon -Force
+
+Remove-ADGroupMember -Identity "Domain Admins" -Members j.anderson 
--- KerberoastingDecoy.ps1
+++ KerberoastingDecoy.ps1
@@ -1,8 +1,8 @@
 # Creating decoy user for Kerberoasting attack
 
 Import-Module ActiveDirectory
-$password = "Password1" | ConvertTo-SecureString -AsPlainText -Force
 
+# Check if decoy user already exists
 try {
     $user = Get-ADUser -Identity j.kelly
     $UserExists = $true
@@ -16,6 +16,26 @@ if ($UserExists) {
     exit
 }
 
+# Generate random password
+Function Get-RandomAlphanumericString {
+	
+	[CmdletBinding()]
+	Param (
+        [int] $length = 32
+	)
+
+	Begin{
+	}
+
+	Process{
+        Write-Output ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $length  | % {[char]$_}) )
+	}	
+}
+
+$password_aux= Get-RandomAlphanumericString | Tee-Object -variable teeTime
+$password = ConvertTo-SecureString -String $password_aux -AsPlainText -Force
+
+# User parameters
 $user = @{
     Name = "John Kelly"
     GivenName = "John"
@@ -30,10 +50,12 @@ $user = @{
     }
 New-ADUser @user
 
-Add-ADGroupMember -Identity "Domain Admins" -Members "j.kelly"
+Add-ADGroupMember -Identity "Domain Admins" -Members "j.kelly"  # to execute an initial login
 
-# To update the value of LastLogon 
+# To update the value of LastLogon to make the decoy user more realistic
 $username = 'COMPANYDOMAIN\j.kelly'
 $credential = New-Object System.Management.Automation.PSCredential $username, $password
-$ActivateLastLogon = Start-Process powershell.exe -Credential $credential -PassThru -ArgumentList '-WindowStyle Hidden'
+$ActivateLastLogon = Start-Process powershell.exe -Credential $credential -PassThru -ArgumentList '-WindowStyle Hidden' -WorkingDirectory 'C:\Windows\System32'
 Stop-Process $ActivateLastLogon -Force
+
+Remove-ADGroupMember -Identity "Domain Admins" -Members j.kelly
